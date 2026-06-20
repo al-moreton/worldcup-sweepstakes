d4d23775bd0d0780bd3f5d85f9ee044d3bb4a901
Alex Moreton <moreton.alexander@gmail.com>
2026-06-20 22:12:39 +0100

    webpack updates

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/index.html"],
-    static: './public',
+    static: "./public",
   },
   plugins: [
     new HtmlWebpackPlugin({
